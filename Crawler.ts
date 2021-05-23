1958b9d81ae82a89ea35e64f63b621ad260f35c8
Heavyrisem <pyo1748@naver.com>
2021-05-23 22:20:41 +0900

    Crawler update

--- Crawler.ts
+++ Crawler.ts
@@ -11,9 +11,9 @@ interface Champion_DB {
 
 interface Champion_position_DB {
     type?: "Top" | "Bottom" | "Middle" | "Jungle" | "Support" | string,
-    mainrune: Precision | Domination | Sorcery | Resolve | Inspiration,
-    subrune: Precision | Domination | Sorcery | Resolve | Inspiration,
-    shards: Array<number>
+    perk: Array<number>,
+    primaryKeyStone: number,
+    subKeyStone: number,
 }
 
 interface Precision {
@@ -91,96 +91,48 @@ function GetChampionRune(characterName: string, position: string): Promise<Champ
         const $ = cheerio.load(res.data);
         
         let Position: Champion_position_DB = {
-            mainrune: {slot1: "", slot2: "", slot3: ""},
-            subrune: {},
-            shards: []
+            primaryKeyStone: 0,
+            subKeyStone: 0,
+            perk: []
         }
         let n = 0;
 
+        $('div.perk-page__item--mark').children('img').each(async (idx, elem) => {
+            if (idx > 1) return;
+            if (idx == 0) Position.primaryKeyStone = parseInt(($(elem).attr('src') as string).replace(/.*(\d{4})\.png.*/, "$1"));
+            if (idx == 1) Position.subKeyStone = parseInt(($(elem).attr('src') as string).replace(/.*(\d{4})\.png.*/, "$1"));
+
+            // console.log(($(elem).attr('src') as string).replace("//opgg-static.akamaized.net/images/lol/perkStyle/", "").replace(/(\.png.*)/, ""))
+        })
+
         $('div.perk-page-wrap').first().children().each(async (idx, elem) => {
 
-
-            if (!$(elem).hasClass("page-divider")) {
-                $(elem).children().children().each(async (idx, elem) => {
-                    
+            if ($(elem).hasClass("perk-page")) {
+                $(elem).children().children().each(async (idx, elem) => { // perk-page__row
+                    // console.log(($(elem).first().children().first().children('img').attr('src') as string).replace(/(\d{4})\.png.*/, "$1"))
                     if ($(elem).hasClass("perk-page__item--active")) {
-                        if (n < 1) {
-                            Position.mainrune.keystone = Build($(elem).first().children().first().children("img").attr("alt")) as any;
-                                n++;
-                        } else {
-                            if (n == 4) Position.subrune.keystone = Build($(elem).first().children().first().children("img").attr("alt")) as any;
-
-                            if (n < 4) {
-                                for (const Key in Position.mainrune) {
-                                    if (!Position.mainrune[Key]) {
-                                        Position.mainrune[Key] = $(elem).first().children().first().children("img").attr("alt") as any;
-                                        break;
-                                    }
-                                }
-                                
-                            } else {
-                                if (idx <= 3) {
-                                    Position.subrune.slot1 = $(elem).first().children().first().children("img").attr("alt") as any;
-                                } else if (idx <= 6) {
-                                    Position.subrune.slot2 = $(elem).first().children().first().children("img").attr("alt") as any;
-                                } else {
-                                    Position.subrune.slot3 = $(elem).first().children().first().children("img").attr("alt") as any;
-                                }
-                            }
-
-                            n++;
-                            // return console.log(n++, $(elem).first().children().first().children("img").attr("alt"), (idx < 6)? parseInt((idx+1)/3+""): 3)
+                        let src = $(elem).first().children().first().children('img').attr('src');
+                        if (src) {
+                            Position.perk.push(parseInt(src.replace(/.*(\d{4})\.png.*/, "$1")));
                         }
-                    }
-
-                    // console.log();
-                    if ($(elem).children().first().children().first().hasClass("active")) {
-                        if ($(elem).children().first().children().first().attr("src")?.indexOf("/5008") != -1) {
-                            Position.shards.push(0);
-                        }
-                        if ($(elem).children().first().children().first().attr("src")?.indexOf("/5001") != -1) {
-                            Position.shards.push(0);
-                        }
-
-                        if ($(elem).children().first().children().first().attr("src")?.indexOf("/5005") != -1) {
-                            Position.shards.push(1);
-                        }
-                        if ($(elem).children().first().children().first().attr("src")?.indexOf("/5002") != -1) {
-                            Position.shards.push(1);
-                        }
-
-                        if ($(elem).children().first().children().first().attr("src")?.indexOf("/5007") != -1) {
-                            Position.shards.push(2);
-                        }
-                        if ($(elem).children().first().children().first().attr("src")?.indexOf("/5003") != -1) {
-                            Position.shards.push(2);
-                        }
-                        n++;
+                            // console.log(src.replace(/.*(\d{4})\.png.*/, "$1"), $(elem).first().children().first().children('img').attr('class'));
                     }
                 })
             }
-            return resolve(Position);
-        })
+
+            if ($(elem).hasClass("fragment-page")) {
+                $(elem).children().first().children("div.fragment").each((idx, elem) => {
+                    let src = $(elem).children().first().children('img').first().attr('src');
+                    if (src) Position.perk.push(parseInt(src.replace(/.*(\d{4})\.png.*/, "$1")));
+                })
+            }
+
+        });
+        // console.log(Position)
+        return resolve(Position)
     })
 }
 
-const Build = (rune?: string) => {
-    if (!rune) return;
-    const List: {
-        [index: string]: any
-    } = {
-        Precision: ["Press the Attack", "Lethal Tempo", "Fleet Footwork", "Conqueror",  "Overheal", "Triumph", "Presence of Mind", "Legend: Alacrity", "Legend: Tenacity", "Legend: Bloodline", "Coup de Grace", "Cut Down", "Last Stand"],
-        Domination: ["Electrocute", "Predator", "Dark Harvest", "Hail of Blades",  "Cheap Shot", "Taste of Blood", "Sudden Impact", "Zombie Ward", "Ghost Poro", "Eyeball Collection", "Ravenous Hunter", "Ingenious Hunter", "Relentless Hunter", "Ultimate Hunter"],
-        Sorcery: ["Summon Aery", "Arcane Comet", "Phase Rush", "Nullifying Orb", "Manaflow Band", "Nimbus Cloak", "Transcendence", "Celerity", "Absolute Focus", "Scorch", "Waterwalking", "Gathering Storm"],
-        Resolve: ["Grasp of the Undying", "Aftershock", "Guardian",  "Demolish", "Font of Life", "Shield Bash", "Conditioning", "Second Wind", "Bone Plating", "Overgrowth", "Revitalize", "Unflinching"],
-        Inspiration: ["Glacial Augment", "Unsealed Spellbook", "Prototype: Omnistone", "Hextech Flashtraption", "Magical Footwear", "Perfect Timing", "Future's Market", "Minion Dematerializer", "Biscuit Delivery", "Cosmic Insight", "Approach Velocity", "Time Warp Tonic"]
-    }
-
-    for (const Rune in List) {
-        if (List[Rune].indexOf(rune) != -1)
-            return Rune;
-    }
-}
 
 function sleep(t:number): Promise<void> {
     return new Promise(resolve => {
@@ -206,27 +158,37 @@ let DB_Client: mongo.MongoClient;
     console.log("Getting HTML data")
     let Version = await axios.default.get("https://ddragon.leagueoflegends.com/api/versions.json");
     Version = Version.data[0];
+    console.log(Version)
     let ChampionList = await axios.default.get(`http://ddragon.leagueoflegends.com/cdn/${Version}/data/ko_KR/champion.json`);
     ChampionList = ChampionList.data.data;
-    
-    let n = 0;
+    // let n = 0;
 
     for (const Champion in ChampionList) {
 
         if (!await DB.collection('Champions').findOne({name: Champion})) {
-            if (n > 2) break;
+            // if (n > 2) break;
             
-            console.log("Loading", Champion)
+            console.log("Adding", Champion)
             let ChampionInfo = await GetChampionData(Champion);
             if (ChampionInfo) {
                 await DB.collection('Champions').insertOne(ChampionInfo);
-                console.log("added", ChampionInfo.name);
+                // console.log("added", ChampionInfo.name);
+                console.log(ChampionInfo)
             } else {
                 console.log("Error while Getting Champion Data", Champion);
             }
         } else {
-            console.log("Skipping", Champion)
-            continue;
+
+            console.log("Updating", Champion);
+            let ChampionInfo = await GetChampionData(Champion);
+            if (ChampionInfo) {
+                await DB.collection('Champions').updateOne({name: Champion}, {$set: ChampionInfo});
+                // console.log("added", ChampionInfo.name);
+            } else {
+                console.log("Error while Getting Champion Data", Champion);
+            }
+            // console.log("Skipping", Champion)
+            // continue;
         }
 
         await sleep(1.8)
